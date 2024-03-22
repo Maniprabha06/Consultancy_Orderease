111fa03af20f2afd458f7d4de2607f03a1fdae72
Maniprabha06 <prabha6603@gmail.com>
2024-03-22 14:09:10 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:orderease_new/navigation_bar.dart';
 import 'package:orderease_new/cartPage.dart';
+import 'package:orderease_new/screens/welcome_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
-      '/': (context) => Navigationbar(),
+      '/': (context) => WelcomeScreen(),
       "cartPage": (context) => CartPage(),
     },
   ));
